5be1910f40a4142b09a2fa3746832d7d92793c7a
Alex P <apputtre@gmail.com>
2026-04-10 01:03:19 -0400

    Created WasRun class

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,9 +2,40 @@
 
 #include "test.h"
 
+class WasRun
+{
+private:
+    std::string name;
+    bool wasRun;
+
+public:
+
+    WasRun(const std::string& name)
+        : name{name}, wasRun{false}
+    {}
+
+    void testMethod()
+    {
+        wasRun = true;
+    }
+
+    std::string getName()
+    {
+        return name;
+    }
+
+    bool getWasRun()
+    {
+        return wasRun;
+    }
+};
+
 int main()
 {
-    assert(2 + 2 == 5, "2 + 2 does not equal 5!");
+    WasRun test("testMethod");
+    assert(test.getWasRun() == 0, "Class reports being run before it has been run");
+    test.testMethod();
+    assert(test.getWasRun() == 1, "Class reports not being after it has been run");
 
     return 0;
 }

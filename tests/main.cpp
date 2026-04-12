c4a925d45c9c5db6759886af8dc31a60bdc559c1
Alex P <apputtre@gmail.com>
2026-04-12 14:53:30 -0400

    Added assert_eq and removed existing implementation of test case

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,67 +2,9 @@
 
 #include "test.h"
 
-class WasRun;
-
-class TestCase
-{
-protected:
-    void (WasRun::* func)();
-
-public:
-    TestCase(void (WasRun::* func)())
-        : func{func}
-    {}
-};
-
-class WasRun : public TestCase
-{
-private:
-    bool wasRun;
-
-public:
-    WasRun(void (WasRun::* func)()) : TestCase(func),
-        wasRun{false}
-    {
-
-    }
-
-    void run()
-    {
-        ((*this).*func)();
-    }
-
-    void testMethod()
-    {
-        wasRun = true;
-    }
-
-    bool getWasRun()
-    {
-        return wasRun;
-    }
-};
-
-class TestCaseTest : public TestCase
-{
-public:
-    TestCaseTest(void (WasRun::* func)()) : TestCase(func)
-    {
-        testRunning();
-    }
-
-    void testRunning()
-    {
-        WasRun test = WasRun(func);
-        assert(test.getWasRun() == 0, "Class reports being run before it has been run");
-        test.run();
-        assert(test.getWasRun() == 1, "Class reports not being after it has been run");
-    }
-};
-
 int main()
 {
-    TestCaseTest test(WasRun::testMethod);
+    std::cout << "Hello world\n";
 
     return 0;
 }

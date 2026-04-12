1d03468fabe87052eb1941926c7f90d391bc9f51
Alex P <apputtre@gmail.com>
2026-04-12 15:05:10 -0400

    Added TestCase class

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,6 +2,15 @@
 
 #include "test.h"
 
+class TestCase
+{
+    public:
+        void setUp();
+        bool run();
+        void tearDown();
+    private:
+};
+
 int main()
 {
     std::cout << "Hello world\n";

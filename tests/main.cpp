561fa168c3c532b275f11930dc511ef8203a9978
Alex P <apputtre@gmail.com>
2026-04-13 15:33:19 -0400

    Moved TestSuite into TestSuite.h

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,6 +7,7 @@
 
 #include "TestSuiteReport.h"
 #include "TestCase.h"
+#include "TestSuite.h"
 
 class TestTestCase : public TestCase
 {
@@ -65,35 +66,6 @@ public:
     void test() override
     {
         assert(2 + 2 == 7, "Two plus two does not equal seven!");
-    }
-};
-
-class TestSuite
-{
-private:
-    std::vector<std::shared_ptr<TestCase>> tests;
-    std::string name;
-    TestSuiteReport report;
-
-public:
-    TestSuite(const std::string name)
-        : name{name}, report{name}
-    {};
-
-    template <typename T>
-    void add(const T& test)
-    {
-        static_assert(std::is_base_of<TestCase, T>::value, "T must derive from TestCase");
-
-        this->tests.emplace_back(std::make_shared<T>(test));
-    }
-
-    TestSuiteReport run()
-    {
-        for (std::shared_ptr<TestCase> test : tests)
-            report.log(test->run());
-        
-        return report;
     }
 };
 

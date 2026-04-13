859ad3619c9803f5c705f1a3a3fe2ecc43491e2b
Alex P <apputtre@gmail.com>
2026-04-13 15:47:26 -0400

    Added TestEnvironment.suite_reports

---
+++ include/TestEnvironment.h
@@ -0,0 +1,44 @@
+#ifndef TESTENVIRONMENT_H
+#define TESTENVIRONMENT_H
+
+#include <string>
+
+#include "TestReport.h"
+#include "TestSuiteReport.h"
+
+class TestEnvironment
+{
+private:
+    std::vector<std::shared_ptr<TestSuiteReport>> suite_reports;
+    TestSuiteReport suite_report;
+    std::unique_ptr<TestReport> curr_test_report;
+
+public:
+    TestEnvironment()
+        : suite_report{"Default"}
+    {}
+
+    void beginTest(const std::string& test_name)
+    {
+        if (curr_test_report)
+            suite_report.log(*curr_test_report);
+
+        curr_test_report = std::make_unique<TestReport>(test_name);
+    }
+
+    void assert(bool statement, const std::string& msg)
+    {
+        if (!statement)
+            curr_test_report->logFailedAssertion(msg);
+    }
+
+    std::string getSummary()
+    {
+        if (curr_test_report)
+            suite_report.log(*curr_test_report);
+
+        return suite_report.getSummary();
+    }
+};
+
+#endif

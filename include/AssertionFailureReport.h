42e05ba15cd4c4ae35c13b2eee88bf58bbd12981
Alex P <apputtre@gmail.com>
2026-04-14 22:51:55 -0400

    Added ComparisonFailureReport

--- include/AssertionFailureReport.h
+++ include/AssertionFailureReport.h
@@ -16,13 +16,27 @@ struct AssertionFailureReport
     }
 };
 
-/*
 template<typename Param1, typename Param2>
 struct ComparisonFailureReport : public AssertionFailureReport
 {
+    enum ComparisonType
+    {
+        EqualTo,
+        NotEqualTo,
+        LessThan,
+        LessThanOrEqualTo,
+        GreaterThan,
+        GreaterThanOrEqualTo
+    };
+
     Param1 x;
     Param2 y;
+    ComparisonType comparisonType;
+
+    std::string getSummary() const
+    {
+        return std::format("Assertion failure (file {}, line {})", location.file_name(), location.line());
+    }
 };
-*/
 
 #endif

eb1b5c6a7a8e920cee5d524e772822c31730ce1b
Alex P <apputtre@gmail.com>
2026-04-21 21:14:44 -0400

    Made TestSuite.name private

--- include/TestSuite.h
+++ include/TestSuite.h
@@ -1,9 +1,17 @@
 #ifndef TESTSUITE_H
 #define TESTSUITE_H
 
+#include <string>
+
 class TestSuite
 {
+private:
+    const std::string name;
 
+public:
+    TestSuite(const std::string& name)
+        : name {name}
+    {}
 };
 
 #endif

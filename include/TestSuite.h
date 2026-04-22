3b315de83f4b3228eec5f1c40c3b1ac0c7f07a69
Alex P <apputtre@gmail.com>
2026-04-21 21:38:17 -0400

    Added TestSuite.tests

--- include/TestSuite.h
+++ include/TestSuite.h
@@ -2,11 +2,18 @@
 #define TESTSUITE_H
 
 #include <string>
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <memory>
+
+#include "TestEnvironment.h"
 
 class TestSuite
 {
 private:
     const std::string name;
+    std::vector<std::shared_ptr<std::function<void(TestEnvironment&)>>> tests;
 
 public:
     TestSuite(const std::string& name)

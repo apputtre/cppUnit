970d2e6a92850d0ad76baef3bf102ea2fa7ae875
Alex P <apputtre@gmail.com>
2026-04-22 17:18:20 -0400

    Laid down framework for new suite syntax

--- test/include/Test9.h
+++ test/include/Test9.h
@@ -1,27 +1,48 @@
 #ifndef TEST9_H
 #define TEST9_H
 
+#include <vector>
+
 #include "TestEnvironment.h"
 #include "TestSuite.h"
 
-class TestSuiteTest : public TestSuite 
+TestEnvironment tenv;
+
+#define SUITE(suite_name, tests)\
+class TestSuite_##suite_name : public TestEnvironment\
+{\
+public:\
+    TestSuite_##suite_name()\
+    {\
+        beginSuite(#suite_name);\
+        tests\
+        endSuite();\
+    }\
+};\
+
+#define TEST(test_name, statements)\
+    beginTest(#test_name);\
+    statements\
+    endTest();\
+
+SUITE(testTestSuite,
 {
-public:
-    TestSuiteTest(const std::string& name) : TestSuite(name)
-    {}
-};
+    TEST(firstTest,
+    {
+        assertEq(2+2, 5, "Two plus two does not equal 5!");
+    })
+
+    TEST(secondTest,
+    {
+        assertEq(std::string("War"), std::string("Peace"), "War is not Peace!");
+    })
+})
 
 TestEnvironment Test9()
 {
     TestEnvironment tenv;
 
-    TestSuiteTest test("Test suite class");
-
-    //auto test_case = [](TestEnvironment& tenv){ tenv.beginTest(); tenv.assertEq(2+2, 5); tenv.endTest(); };
-
-    //test.addTest(test_case);
-
-    test.run(tenv);
+    std::cout << TestSuite_testTestSuite().getSummary() << std::endl;
 
     return tenv;
 }

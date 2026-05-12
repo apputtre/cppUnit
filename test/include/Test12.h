63b5f324b645c0e818b540df6658c53e2b56f8b9
Alex P <apputtre@gmail.com>
2026-05-12 16:06:25 -0400

    Changed how registerTest works
    Also removed TestRunner.

--- test/include/Test12.h
+++ test/include/Test12.h
@@ -1,16 +1,5 @@
 #include "yUnit.h"
 
-struct TestRunner
-{
-    template<std::derived_from<TestEnvironment> T_TestEnvironment>
-    TestRunner(T_TestEnvironment& tenv, void(T_TestEnvironment::*test_func)())
-    {
-        tenv.setUp();
-        (tenv.*test_func)();
-        tenv.tearDown();
-    }
-};
-
 class TestEnvironmentSubclass : public TestEnvironment
 {
 public:
@@ -49,7 +38,8 @@ SUITE(registerTest,
         assert(!tenv.ran, "\'ran\' flag was true before running test!");
         assertEq(tenv.log, std::string(""), "Log is not empty before running test!");
 
-        TestRunner trun(tenv, tenv.test);
+        tenv.registerTest(tenv.test);
+        tenv.runTests();
 
         assert(tenv.ran, "\'run\' flag was not set!");
         assertEq(tenv.log, std::string("setUp test tearDown "));

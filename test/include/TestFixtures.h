9c8570a05514cf89c8bb740024dfb87a75e8274a
Alex P <apputtre@gmail.com>
2026-05-19 14:37:37 -0400

    Added test for fixtures

---
+++ test/include/TestFixtures.h
@@ -0,0 +1,33 @@
+#ifndef TESTFIXTURES_H
+#define TESTFIXTURES_H
+
+#include "yUnit.h"
+
+FIXTURE()
+{
+    std::string log;
+
+    SETUP()
+    {
+        log += "setUp() "
+    }
+
+    TEARDOWN()
+    {
+        log += "tearDown() "
+    }
+
+    FTEST("First test")
+    {
+        assertEq(2+2, 5, "Two plus two does not equal five!");
+
+        log += "test_1 "
+    }
+
+    FTEST("Second test")
+    {
+        assertEq(log, "setUp() test_1 tearDown() ", "Log is not correct!");
+    }
+}
+
+#endif
